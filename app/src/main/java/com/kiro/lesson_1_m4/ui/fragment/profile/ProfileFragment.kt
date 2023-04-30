54d7f912d13f76c2258230312f15f86698714837
kamola25 <kamolaboribaevak13@gmail.com>
2023-04-30 05:49:22 +0300

    add 4hm

---
+++ app/src/main/java/com/kiro/lesson_1_m4/ui/fragment/profile/ProfileFragment.kt
@@ -0,0 +1,22 @@
+package com.kiro.lesson_1_m4.ui.fragment.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kiro.lesson_1_m4.R
+
+class ProfileFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+
+}
